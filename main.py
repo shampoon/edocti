ce105a1e7442f0c41b821a2cc65e3497d81a3065
Vadim <shampoonvs@gmail.com>
2021-08-13 09:15:29 +0500

    #2 задача решена. иземенения затронули файл main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 print('Hello world!')
-
+#2 я програмист №2 решаю задачу №2
+a = 4
+b = 5
+c = (a ** 2 + b ** 2) ** 0.5
 print('Hello world!')
-
+print(c)
 print('Hello world!')
 
 print('Hello world!')
