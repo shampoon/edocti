7d0452732d6c03545bfe914394b2180ce61248f7
Vadim <shampoonvs@gmail.com>
2021-08-13 09:06:32 +0500

    #1 решил задачу №1 внес изменения в main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-print('Hellow world!')
+print('Hello world!')
+#1 я решаю задачу №1 и делаю пишу тут код
+a = [i for i in 'abcdefg']
+print(a)
+print('Hello world!')
+#1 тут я тоже что-то решаю...
+b=a
+print('Hello world!')
 
-print('Hellow world!')
+print('Hello world!')
 
-print('Hellow world!')
-
-print('Hellow world!')
-
-print('Hellow world!')
+print('Hello world!')
